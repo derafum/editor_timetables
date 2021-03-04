f37e1758908d8cb05b0a0b542f96839ff0712c77
nodax-hub <76558792+nodax-hub@users.noreply.github.com>
2021-03-05 00:55:06 +0400

    Немного переписал функциию для поиска смены на удаление. Добавил две функции для тестов и для запуска уже готового результата. (Режим debug)

--- main.py
+++ main.py
@@ -34,22 +34,37 @@ def concatenate_lists(lists):
 
 class Main:
     def __init__(self):
+        self.start()
+
+    def debug(self):
+        self.task = False  # self.task = True if input('Спрашивать ужимать дальше и какую смену удалять [y/n]: ') == 'y' else False
+        self.number_of_people = 10  # self.number_of_people = int(input('Введите количество человек: '))
+        self.people_in_shift = 6  # self.people_in_shift = int(input('Введите количество человек в смене: '))
+        self.list_people = generation_list(self.number_of_people)
+
+        self.name_file = 'import_10_6'  # input('Введите имя файла [имя по умолчанию: import]: ')
+        self.name_file = 'import' if self.name_file == '' else self.name_file
+        with open(self.name_file + '.csv') as file:
+            reader = csv.reader(file, delimiter=';', quotechar=',', quoting=csv.QUOTE_MINIMAL)
+            self.schedule = [list(map(int, row)) for row in reader]
+
+        self.all_pairs = get_a_combination(self.list_people)
+        self.all_shifts = get_a_combination(self.list_people, self.people_in_shift)
+        self.schedule_list = []
+
+        self.run(self.schedule)
+
+    def start(self):
         self.task = True if input('Спрашивать ужимать дальше и какую смену удалять [y/n]: ') == 'y' else False
         self.number_of_people = int(input('Введите количество человек: '))
         self.people_in_shift = int(input('Введите количество человек в смене: '))
         self.list_people = generation_list(self.number_of_people)
 
-        if input('Хотите ли вы импортировать данные [y/n]: ') == 'y':
-            with open('import.csv') as file:
-                reader = csv.reader(file, delimiter=';', quotechar=',', quoting=csv.QUOTE_MINIMAL)
-                self.schedule = [list(map(int, row)) for row in reader]
-        else:
-            self.schedule = [
-                [1, 2, 3, 6, 7, 8],
-                [1, 2, 4, 5, 7, 9],
-                [1, 3, 5, 6, 9, 10],
-                [2, 4, 6, 7, 8, 10],
-                [3, 4, 5, 6, 8, 9]]
+        self.name_file = input('Введите имя файла [имя по умолчанию: import]: ')
+        self.name_file = 'import' if self.name_file == '' else self.name_file
+        with open(self.name_file + '.csv') as file:
+            reader = csv.reader(file, delimiter=';', quotechar=',', quoting=csv.QUOTE_MINIMAL)
+            self.schedule = [list(map(int, row)) for row in reader]
 
         self.all_pairs = get_a_combination(self.list_people)
         self.all_shifts = get_a_combination(self.list_people, self.people_in_shift)
@@ -59,7 +74,7 @@ class Main:
 
     def run(self, schedule):
         print('Начинаю ужимать расписание:')
-        [print(i+1, shift) for i, shift in enumerate(schedule)]
+        [print(i + 1, shift) for i, shift in enumerate(schedule)]
         print()
         # Основной алгоритм (ужатие)
         run = True
@@ -71,14 +86,14 @@ class Main:
             if count_unmet != 0:
                 # Пробуем изменять смены (пристаивая пары)
                 changed = False
-                #Список не встретившихся людей
+                # TODO
+                # Список не встретившихся людей
                 self.unmet_people = self.find_unmet_people(schedule)
-                # Определяем список смен в которых люди из не встретившихся пар
-                shifts_index = self.find_shifts_with_unmet_people(schedule)
                 # Составляем список всех смен содержащих невстретившихся людей
-                self.all_shifts_with_unmet_people = [self.all_shifts[i] for i in self.find_shifts_with_unmet_people(self.all_shifts)]
-                # Для каждой найдой смены
-                for index in shifts_index:
+                self.all_shifts_with_unmet_people = [self.all_shifts[i] for i in
+                                                     self.find_shifts_with_unmet_people(self.all_shifts)]
+                # Пробуем пристроить пару в одну из смен
+                for index in range(len(schedule)):
                     # Пробуем уменьшить кол-во не встретившихся
                     test_schedule = self.change_shift(schedule, index)
                     # Если найденна смена с которой не встретившихся пар меньше меняем в расписании эту смену
@@ -93,25 +108,24 @@ class Main:
             else:
                 self.schedule_list.append(schedule.copy())
                 if self.task:
-                    [print(i+1, shift) for i, shift in enumerate(schedule)]
+                    [print(i + 1, shift) for i, shift in enumerate(schedule)]
                     if input('Ужимать? [y/n]: ') == 'y':
-                        shifts_index = self.calc_index_shifts(schedule)
-                        if len(shifts_index)>1:
-                            shift_index = int(input('Выберите одну из смен для удаления {}: '.format( list(map(lambda x: x+1, shifts_index)))) ) - 1
+                        shift_index = self.calc_index_shifts(schedule)
+                        if len(shift_index) > 1:
+                            shift_index = int(input('Выберите одну из смен для удаления {}: '.format(
+                                list(map(lambda x: x + 1, shift_index))))) - 1
                         else:
-                            shift_index = shifts_index[0]
+                            shift_index = shift_index[0]
                         print('Удаляю {} {} смену'.format(shift_index + 1, schedule.pop(shift_index)))
                     else:
                         break
                 else:
-                    # [print(i+1, shift) for i, shift in enumerate(schedule)]
                     shift_index = self.calc_index_shifts(schedule)[0]
-                    # print('Удаляю {} смену'.format(shift_index+1))
                     schedule.pop(shift_index)
         # print('Список всех промежуточных состояний: {}'.format(self.schedule_list))
         print('Результат работы программы:')
-        [print(i+1, shift) for i, shift in enumerate(self.schedule_list[-1])]
-        # input('Для выхода из программы нажмите Enter')
+        [print(i + 1, shift) for i, shift in enumerate(self.schedule_list[-1])]
+        input('Для выхода из программы нажмите Enter')
         exit()
 
     def find_unmet(self, schedule):
@@ -134,6 +148,7 @@ class Main:
 
     def change_shift(self, schedule, index):
         """Функция перебирает все смены содержащие невстретившихся людей для выбранной и заменяет на ту в которой кол-во невстретившихся меньше"""
+        # TODO
         test_schedule = schedule.copy()
         test_schedule.pop(index)
         for shift in self.all_shifts_with_unmet_people:
@@ -149,24 +164,16 @@ class Main:
         # Пары для каждой смены образовавшиеся в нашем (переданном в функцию) расписании
         couples_in_shifts = find_pairs(schedule)
         # Кол-во встреч каждой из пар (словарь пара:кол-во)
-        number_of_meetings = count_elements(list(chain(*couples_in_shifts)), self.all_pairs)
-        # минимальное кол-во встреч
-        minimum_number_of_meetings = min(list(filter(lambda x: x != 0, number_of_meetings.values())))
-        # Список пар кол-во встреч которых минимально
-        couples_with_min_number_meetings = [pair for pair, quantity in number_of_meetings.items() if
-                                            quantity == minimum_number_of_meetings]
-        # Количество пар встречавшихся наименьшее количество раз для каждой смены
-        number_of_couples_with_min_meetings = [
-            sum(count_elements(pair, couples_with_min_number_meetings).values()) for
-            pair in couples_in_shifts]
-        # Список номеров смен с наименьшим количеством пар встретившихся мин кол-во раз
-        list_of_shift_numbers = [i for i, value in enumerate(number_of_couples_with_min_meetings) if
-                                 value == min(number_of_couples_with_min_meetings)]
-        return list_of_shift_numbers
+        number_of_meetings = count_elements(concatenate_lists(couples_in_shifts), self.all_pairs)
+        # Таблица: Кол-во пар в сменах которые встречались только один раз.
+        table = [[number_of_meetings[pair] for pair in shift].count(1) for shift in couples_in_shifts]
+        # Выбираем из неё те смены в которых таких пар минимально
+        shift_numbers = [i for i, j in enumerate(table) if j == min(table)]
+        return shift_numbers
 
     def save_to_file(self):
         """Функция сохраняет данные в файл"""
-        print(self)
+        pass
 
 
 if __name__ == '__main__':
