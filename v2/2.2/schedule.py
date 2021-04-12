3fb96842f8c36b1ccc2e457ea80e9ac429104c1c
nodax-hub <ioann.ishenko.2003@gmail.com>
2021-04-12 21:49:09 +0400

    Разбил свойство мин кол-ва пар встречавшихся раз на несколько свойств (попыталься добавить функцию изменения - требует доработки)

--- v2/2.2/schedule.py
+++ v2/2.2/schedule.py
@@ -50,22 +50,28 @@ class Schedule:
         self.changed = False
 
         # Вычисляемые свойсва класса:
+        self.__couples_meeting_only_one_at_a_time = None  # Пары кол-во встреч которых = 1
+        self.__couples_in_shifts = None  # Пары в каждой из смен
+        self.__number_of_meetings = None  # Кол-во встреч каждой из пар
         self.__count_shifts = None  # Кол-во смен в расписании
         self.__unmet_couples = None  # Не встретившиеся пары
         self.__count_unmet = None  # Кол-во не встретившихся
         self.__unmet_people = None  # Не встретившиеся
         self.__shifts_with_unmet_people = None  # Смены в которых есть не встретившиеся
-        self.__abbreviated_shifts = None  # Смены которые можно сократить
+        self.__shifts_in_which_the_number_of_pairs_met_once_is_minimal = None  # Смены которые можно сократить
 
     def update(self, shifts):
         """Затирает все посчитанные значения (чтоб они пересчитались) и обновляет смены"""
         self.shifts = shifts[:]
+        self.__couples_meeting_only_one_at_a_time = None
+        self.__couples_in_shifts = None
+        self.__number_of_meetings = None
         self.__count_shifts = None
         self.__unmet_couples = None
         self.__count_unmet = None
         self.__unmet_people = None
         self.__shifts_with_unmet_people = None
-        self.__abbreviated_shifts = None
+        self.__shifts_in_which_the_number_of_pairs_met_once_is_minimal = None
 
     def read(self):
         """Выводит расписание в консоль"""
@@ -85,13 +91,41 @@ class Schedule:
         # TODO Придумать как выбрать единственно верную смену под сокращение
         # Сокращаем первую смену из списка смен под сокращение
         shifts = self.shifts[:]
-        shifts.pop(self.abbreviated_shifts[0])
+        shifts.pop(self.shifts_in_which_the_number_of_pairs_met_once_is_minimal[0])
         self.update(shifts)
 
     def change(self):
         """Занимается изменением расписания"""
-        # TODO Написать код
-        pass
+        print(self.unmet_couples)
+        self.changed = False
+        index = self.shifts_in_which_the_number_of_pairs_met_once_is_minimal[0]
+        people_for_change = self.list_people[:]
+        shift = self.shifts[index]
+        remaining_shifts = self.shifts[:]
+        remaining_shifts.pop(index)
+        [people_for_change.pop(people_for_change.index(people)) for people in shift if
+         people in self.unmet_people and people in people_for_change]
+        pairs = [pair for pair in self.couples_in_shifts[index] if pair in self.couples_meeting_only_one_at_a_time]
+        [people_for_change.pop(people_for_change.index(people)) for people in set(concatenate_lists(pairs)) if
+         people in people_for_change]
+        people_under_change = set(people_for_change) & set(shift)
+        print(index, shift)
+        print(people_under_change, people_for_change)
+        for u in people_under_change:
+            for f in people_for_change:
+                test_shift = shift[:]
+                test_shift.pop(test_shift.index(u))
+                test_shift.append(f)
+                test_shift.sort()
+                print(remaining_shifts + [test_shift])
+                test_schedule = Schedule([self.number_of_people, self.people_in_shift],
+                                         remaining_shifts + [test_shift])
+                test_schedule.read()
+                print('*', test_schedule.count_unmet, self.count_unmet)
+                if test_schedule.count_unmet < self.count_unmet:
+                    self.changed = True
+                    self.update(test_schedule.shifts[:])
+                    break
 
     @property
     def count_shifts(self):
@@ -132,17 +166,36 @@ class Schedule:
         return self.__shifts_with_unmet_people
 
     @property
-    def abbreviated_shifts(self):
-        """Список смен которые можно сократить
-        Находит смены в которых меньше всего пар встречавшихся 1 раз"""
-        if self.__abbreviated_shifts is None:
-            # Пары для каждой смены образовавшиеся в нашем (переданном в функцию) расписании
-            couples_in_shifts = find_pairs(self.shifts)
-            # Кол-во встреч каждой из пар (словарь пара:кол-во)
-            number_of_meetings = count_elements(concatenate_lists(couples_in_shifts), self.all_pairs)
-            # Таблица: Кол-во пар в сменах которые встречались только один раз.
-            table = [[number_of_meetings[pair] for pair in shift].count(1) for shift in couples_in_shifts]
+    def couples_meeting_only_one_at_a_time(self):
+        """Пары которые встречались только один раз"""
+        if self.__couples_meeting_only_one_at_a_time is None:
+            self.__couples_meeting_only_one_at_a_time = [pair for pair in self.all_pairs if
+                                                         self.number_of_meetings[pair] == 1]
+        return self.__couples_meeting_only_one_at_a_time
+
+    @property
+    def couples_in_shifts(self):
+        """Пары для каждой смены образовавшиеся в нашем расписании"""
+        if self.__couples_in_shifts is None:
+            self.__couples_in_shifts = find_pairs(self.shifts)
+        return self.__couples_in_shifts
+
+    @property
+    def number_of_meetings(self):
+        """Кол-во встреч каждой из пар (словарь пара:кол-во)"""
+        if self.__number_of_meetings is None:
+            self.__number_of_meetings = count_elements(concatenate_lists(self.couples_in_shifts), self.all_pairs)
+        return self.__number_of_meetings
+
+    @property
+    def shifts_in_which_the_number_of_pairs_met_once_is_minimal(self):
+        """Находит смены в которых меньше всего пар встречавшихся один раз"""
+        if self.__shifts_in_which_the_number_of_pairs_met_once_is_minimal is None:
+            # Кол-во пар в сменах которые встречались только один раз.
+            table = [[self.number_of_meetings[pair] for pair in shift].count(1) for shift in self.couples_in_shifts]
+            # Минимальное кол-во пар в сменах
+            min_z = min(table)
             # Выбираем из неё те смены в которых таких пар минимально
-            shift_numbers = [i for i, j in enumerate(table) if j == min(table)]
-            self.__abbreviated_shifts = shift_numbers
-        return self.__abbreviated_shifts
+            shift_numbers = [i for i, j in enumerate(table) if j == min_z]
+            self.__shifts_in_which_the_number_of_pairs_met_once_is_minimal = shift_numbers
+        return self.__shifts_in_which_the_number_of_pairs_met_once_is_minimal
