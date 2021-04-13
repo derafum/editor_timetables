1152b5ca464a4ab74c5c9ae54df20a04f240f2ce
nodax-hub <ioann.ishenko.2003@gmail.com>
2021-04-14 01:08:55 +0400

    ничего особо не поменял

--- v2/2.2/main_v2.2.py
+++ v2/2.2/main_v2.2.py
@@ -4,22 +4,26 @@ from get_schedule import get_schedule
 def main(schedule):
     """Основной алгоритм"""
     while True:
-        schedule.read()
         # Если все встретились
         if schedule.count_unmet == 0:
             # Сохраняем и сокращаем расписание
+            print('все встретились')
+            # schedule.read()
             schedule.save()
             schedule.cut()
         else:
             # Пробуем изменить расписание
             schedule.change()
-            # Если не вышло
+            # Если не вышло завершаем работу программы
             if not schedule.changed:
-                # Возвращаем последний рабочий вариант
+                # Восстанавливаем последний рабочий вариант
                 schedule.load()
                 break
+    print()
+    print('Результат:')
+    schedule.read()
     return schedule.shifts
 
 
 if __name__ == '__main__':
-    print(main(get_schedule()))
+    main(get_schedule())
