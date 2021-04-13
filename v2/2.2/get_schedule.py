ad3345e54c7d5edb512ce3c916e3a389a3a83071
nodax-hub <ioann.ishenko.2003@gmail.com>
2021-04-14 01:07:00 +0400

    поставил по в режиме отладки 6 из 20

--- v2/2.2/get_schedule.py
+++ v2/2.2/get_schedule.py
@@ -6,8 +6,8 @@ debug = True
 
 def get_schedule():
     if debug:
-        people_in_total_and_in_shift = [10, 6]
-        name_file = '../../data/import_10_6'
+        people_in_total_and_in_shift = [20, 6]
+        name_file = '../../data/import_20_6'
         with open(name_file + '.csv') as file:
             reader = csv.reader(file, delimiter=';', quotechar=',', quoting=csv.QUOTE_MINIMAL)
             schedule = Schedule(people_in_total_and_in_shift, [list(map(int, row)) for row in reader])
