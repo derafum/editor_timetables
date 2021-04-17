05d1ade45365fe4d0bb5c131687e1467d7bbea80
nodax-hub <ioann.ishenko.2003@gmail.com>
2021-04-17 17:08:40 +0400

    избавился от ввода кол-ва людей и кол-ва в смене

--- v2/2.2/get_user_data.py
+++ v2/2.2/get_user_data.py
@@ -1,34 +1,29 @@
 from schedule import Schedule
 from settings import *
-import csv
 
 
 def get_data():
     if DEBUG:
-        people_in_total_and_in_shift = [20, 6]
-        name_file = '../../data/import_20_6'
-        with open(name_file + '.csv') as file:
-            reader = csv.reader(file, delimiter=';', quotechar=',', quoting=csv.QUOTE_MINIMAL)
-            schedule = Schedule(people_in_total_and_in_shift, [list(map(int, row)) for row in reader])
-        name_to_save = 'test'
+        with open('../../data/import_20_6.csv') as file:
+            lines = file.read().splitlines()
+            schedule = Schedule([list(map(int, line.split(';'))) for line in lines])
+        name_to_save = 'debug_export'
     else:
-        people_in_total_and_in_shift = list(map(int, [input('Введите количество человек: '),
-                                                      input('Введите количество человек в смене: ')]))
         name_file = input('Введите имя импортируемого файла [.csv]: ')
         with open(name_file + '.csv') as file:
-            reader = csv.reader(file, delimiter=';', quotechar=',', quoting=csv.QUOTE_MINIMAL)
-            schedule = Schedule(people_in_total_and_in_shift, [list(map(int, row)) for row in reader])
+            lines = file.read().splitlines()
+            shifts = [list(map(int, line.split(';'))) for line in lines]
+            schedule = Schedule(shifts)
         name_to_save = input('Введите имя файла для сохранения результата [.csv]: ')
 
-    return schedule, name_to_save + '_' + '_'.join(map(str, people_in_total_and_in_shift))
+    return schedule, name_to_save
 
 
 def save_to_csv(name, timetables):
-    with open(name + '.csv', 'w', newline='') as isfile:
-        writer = csv.writer(isfile, delimiter=";")
+    with open(name + '.csv', 'w') as file:
         for i, schedule in enumerate(timetables, 1):
             if len(timetables) > 1:
-                writer.writerow(['Расписание № ' + str(i)])
+                file.write('Расписание № ' + str(i) + '\n')
             for shift in schedule:
-                writer.writerow(shift)
-            writer.writerow([''])
+                file.write(';'.join(map(str, shift)) + '\n')
+            file.write('\n')
