338b438383fa2c4a9e2cce3974f56b9aaf8ebf94
nodax-hub <ioann.ishenko.2003@gmail.com>
2021-04-17 17:09:12 +0400

    добавил проверку

--- Test and create new functions/my_db/main_db.py
+++ Test and create new functions/my_db/main_db.py
@@ -1,11 +1,19 @@
+import os
+
 name = 'test_db.txt'
 
 
 def save(mass):
-    with open(name, mode='a') as f:
+    with open(name, 'a') as f:
         f.write(str(mass) + '\n')
 
 
 def load():
-    with open(name, mode='r') as f:
+    with open(name) as f:
         return [eval(elem) for elem in f.read().splitlines()]
+
+
+[save(list(range(1, 10 + 1))) for i in range(5)]
+print(load())
+os.remove(name)
+input()
